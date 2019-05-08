2e6ba5b772e3bbb483c1c10fdc6dd9be77bed8fc
张路 <zhanglu30@jd.com>
2019-05-08 18:01:21 +0800

    添加uikitex方法

--- LZKit.podspec
+++ LZKit.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'LZKit'
-  s.version          = '0.1.0'
+  s.version          = '0.1.1'
   s.summary          = 'LZKit is a test lib'
 
 # This description is used to generate tags and improve search results.
--- LZKit/Classes/UIKitEx.swift
+++ LZKit/Classes/UIKitEx.swift
@@ -8,6 +8,58 @@
 import Foundation
 import UIKit
 
+public protocol StoryBoardLoadable {
+    
+}
+
+extension StoryBoardLoadable {
+    public static func loadVCFromStoryBoard() -> Self {
+        return UIStoryboard(name: "\(self)", bundle: nil).instantiateViewController(withIdentifier: "\(self)") as! Self
+    }
+}
+
+public protocol NibLoadable {
+    
+}
+
+extension NibLoadable {
+    public static func loadViewFromNib() -> Self {
+        return Bundle.main.loadNibNamed("\(self)", owner: nil, options: nil)?.first as! Self
+    }
+}
+
+
+public protocol RegisterCellFromNib {
+    
+}
+
+extension RegisterCellFromNib {
+    
+    public static var identifiy: String {
+        return "\(self)"
+    }
+    
+    public static var nib: UINib? {
+        return UINib.init(nibName: "\(self)", bundle: nil)
+    }
+}
+
+extension UITableView {
+    
+    public func lz_registerNibCell<T: UITableViewCell>(cell: T.Type) where T: RegisterCellFromNib {
+        if let cellNib = T.nib {
+            self.register(cellNib, forCellReuseIdentifier: T.identifiy)
+        } else {
+            self.register(cell, forCellReuseIdentifier: "123")
+        }
+        
+    }
+    
+    public func lz_dequeueReusableCell<T: UITableViewCell>() -> T where T: RegisterCellFromNib {
+        return self.dequeueReusableCell(withIdentifier: T.identifiy) as! T
+    }
+}
+
 extension UIView {
     
     public var x: CGFloat {
@@ -55,3 +107,4 @@ extension UIView {
     }
     
 }
+
